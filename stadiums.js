ac2738c77b74c6e94ce73e6a5b3d6a050fb9730d
Rickey <fiter@ashland.or.us>
2016-03-24 09:11:42 -0700

    Update stadiums.js

--- stadiums.js
+++ stadiums.js
@@ -6,6 +6,8 @@ You can use this to find the Lat/Long http://timw1984.github.io/WebApp-Location/
 tex: Name, City and State of the stadium. This will be used in the bottom left of the App to describe what the user is looking at
 linki: This will be used in the wikipedia search to find the stadium. i.e. https://en.wikipedia.org/w/index.php?title=Special:Search&search=Reser%20Stadium 
 If you could test if the link works with the linki string, I would appreciate it.
+Look here for stadium list: 
+https://en.wikipedia.org/wiki/List_of_American_football_stadiums_by_capacity
 */
 var CFData = [ 
 				{name:"OregonState", lat:"44.55952", longi:"-123.28147",tex:"Reser Stadium, Corvallis, Oregon", linki:"Reser Stadium"}, 
@@ -16,20 +18,21 @@ var CFData = [
 				{name:"UoTennessee", lat:"35.955", longi:"-83.925",tex:"Neyland Stadium, Knoxville, Tennessee", linki:"Neyland Stadium"}, 
 				{name:"LAState", lat:"30.411944", longi:"-91.185556",tex:"Tiger Stadium, Baton Rouge, Louisiana", linki:"Tiger Stadium"}, 
 				{name:"UoAlabama", lat:"33.207778", longi:"-87.550556",tex:"Bryant-Denny Stadium, Tuscaloosa, Alabama", linki:"Bryant-Denny Stadium"}, 
-				{name:"UoTexas", lat:"30.283611", longi:" -97.7325",tex:"Darrell K. Royal–Texas Memorial Stadium, Austin, TX", linki:"Darrell K. Royal–Texas Memorial Stadium"}, 
+				{name:"UoTexas", lat:"30.283611", longi:" -97.7325",tex:"Darrell K. Royalâ€“Texas Memorial Stadium, Austin, TX", linki:"Darrell K. Royalâ€“Texas Memorial Stadium"}, 
 				{name:"UoNebraska", lat:"40.820556", longi:"-96.705556",tex:"Memorial Stadium, Lincoln, Nebraska", linki:"Memorial Stadium"}, 
 				{name:"USC", lat:"34.014167", longi:"-118.287778",tex:"Los Angeles Memorial Coliseum, Los Angeles, California", linki:"Los Angeles Memorial Coliseum"}, 
 				{name:"UoGeorgia", lat:"33.949722", longi:"-83.373333",tex:"Sanford Stadium, Athens, GA", linki:"Sanford Stadium"}, 
 				{name:"UCLA", lat:"34.161389", longi:"-118.1675",tex:"Rose Bowl, Pasadena, California", linki:"Rose Bowl"}, 
 				{name:"CottonBowl", lat:"32.779722", longi:"-96.759722",tex:"Cotton Bowl, Fair Park, Dallas, Texas", linki:"Cotton Bowl"}, 
 				{name:"UoFlorida", lat:"29.65", longi:"-82.348611",tex:"Ben Hill Griffin Stadium at Florida Field, Gainesville, Floridas", linki:"Ben Hill Griffin Stadium at Florida Field"}, 
-				{name:"Auburn", lat:"32.602222", longi:"-85.489167",tex:"Jordan–Hare Stadium, Auburn, Alabama", linki:"Jordan–Hare Stadium"}, 
+				{name:"Auburn", lat:"32.602222", longi:"-85.489167",tex:"Jordanâ€“Hare Stadium, Auburn, Alabama", linki:"Jordanâ€“Hare Stadium"}, 
 				{name:"FloridaState", lat:"30.438056", longi:"-84.304444",tex:"Doak Campbell Stadium, Tallahassee, Florida", linki:"Doak Campbell Stadium"}, 
 				{name:"UoOklahoma", lat:"35.205833", longi:"-97.4425",tex:"Gaylord Family Oklahoma Memorial Stadium, Norman, Oklahoma", linki:"Gaylord Family Oklahoma Memorial Stadium"}, 
 				{name:"Clemson", lat:"34.678611", longi:"-82.843056",tex:"Memorial Stadium, Clemson, South Carolina", linki:"Memorial Stadium"}, 
 				{name:"NotreDame", lat:"41.698", longi:"-86.234",tex:"Notre Dame Stadium, South Bend, Indiana", linki:"Notre Dame Stadium"}, 
 				{name:"UoSCarolina", lat:"33.973056", longi:"-81.019167",tex:"Williams-Brice Stadium, Columbia, South Carolina", linki:"Williams-Brice Stadium"}, 
-				{name:"MichiganState", lat:"42.728056", longi:"-84.484722",tex:"Spartan Stadium, East Lansing, MI", linki:"Spartan Stadium"} 
+				{name:"MichiganState", lat:"42.728056", longi:"-84.484722",tex:"Spartan Stadium, East Lansing, MI", linki:"Spartan Stadium"},
+					{name:"UoWashington", lat:"47.650278", longi:"-122.301667",tex:"Husky Stadium, Seattle, WA", linki:"Husky Stadium"} //I only did one to make sure I know how to do this, I will work on it more later
 			]; 
 	
 	
@@ -58,7 +61,7 @@ var CFData = [
 	<option class="CFB" value="UoSCarolina">University of South Carolina</option>
 	<option class="CFB" value="UoTennessee">University of Tennessee</option>
 	<option class="CFB" value="UoTexas">University of Texas</option>
-	
+	<option class="CFB" value="UoWashington">University of Washinton</option>	
 	
 	
 	
